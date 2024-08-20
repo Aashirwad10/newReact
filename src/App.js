d56d27cfffc6d00fea756401a645c4c95a4f8409
Aashirwad10 <aashbudh10@gmail.com>
2024-08-20 17:54:18 +0545

    4 home pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import HomePage from './pages/HomePage';
 import NavigationBar from './components/NavigationBar';
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
+import ThirdPage from './pages/ThirdPage';
+import FourthPage from './pages/FourthPage';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
-      <NavigationBar />
+      <FirstPage />
+      <SecondPage />
+      <ThirdPage />
+      <FourthPage />
+      <NavigationBar /> 
     </div>
   );
 }
---
+++ src/pages/SecondPage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../styles/FirstPage.css'; 
+
+import chicken from '../assets/chicken.jpg';
+import healthy from '../assets/healthy.jpg';
+import quick from '../assets/quick.jpg';
+import salad from '../assets/salad.jpg';
+
+const SecondPage = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>
+          Trending <span className="highlight">now.</span>
+        </h1>
+      </div>
+
+      <div className="recipe-grid">
+        <div className="recipe-card">
+          <img src={quick} alt="Quicky" />
+          <div className="label">Quicky</div>
+        </div>
+        <div className="recipe-card">
+          <img src={chicken} alt="Popular" />
+          <div className="label">Popular</div>
+        </div>
+        <div className="recipe-card">
+          <img src={salad} alt="SALAD" />
+          <div className="label">SALAD</div>
+        </div>
+        <div className="recipe-card">
+          <img src={healthy} alt="Healthy" />
+          <div className="label">Healthy</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondPage;
